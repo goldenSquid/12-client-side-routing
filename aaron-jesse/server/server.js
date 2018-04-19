fa4eaa08958371b3802d73e1ada494ee203ec610
Jesse Atay <jesse.atay@gmail.com>
2018-04-18 17:53:30 -0700

    initialized file tree

--- aaron-jesse/server/server.js
+++ aaron-jesse/server/server.js
@@ -10,9 +10,10 @@ const pg = require('pg');
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
